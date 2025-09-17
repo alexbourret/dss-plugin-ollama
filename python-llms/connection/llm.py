bc940e6a32eb8ce52564fab5006e11320946d0ba
admin <admin@localhost>
2025-09-17 18:06:08 +0200

    Edited file 'python-llms/connection/llm.py'

--- python-llms/connection/llm.py
+++ python-llms/connection/llm.py
@@ -3,16 +3,28 @@ from dataiku.llm.python import BaseLLM
 
 class OllamaLLM(BaseLLM):
     def __init__(self):
-        pass
+        print("ALX:OllamaLLM init")
 
     def set_config(self, config: dict, plugin_config: dict) -> None:
-        pass
+        print("ALX:OllamaLLM set_config")
 
     def process(self, query, settings, trace):
         print("ALX:query={}, settings={}".format(query, settings))
         prompt = query["messages"][0]["content"]
 
         # Add code here, for LLM completion, tool execution, etc...
+        # data = {
+        #   "model": "{}".format(model_name),
+        #   "prompt": "{}".format(prompt)
+        #  }
+        # if image:
+        #     data["images"] = [image]
+        # if output_format:
+        #     data["format"] = output_format
+        # try:
+        #     response = requests.post(url="{}/api/generate".format(server_url), json=data)
+        # except Exception as error:
+        #     raise Exception("Connection error: {}".format(error))
 
         return {
             "text": "the custom LLM answer",
