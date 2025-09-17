96b1d127c7226a503951d211904e692e02f5dd62
admin <admin@localhost>
2025-09-17 17:47:22 +0200

    Added new custom model 'connection'

---
+++ python-llms/connection/llm.py
@@ -0,0 +1,23 @@
+# This file is the implementation of custom LLM connection
+from dataiku.llm.python import BaseLLM
+
+
+class MyLLM(BaseLLM):
+    def __init__(self):
+        pass
+
+    def set_config(self, config: dict, plugin_config: dict) -> None:
+        pass
+
+    def process(self, query, settings, trace):
+        prompt = query["messages"][0]["content"]
+
+        # Add code here, for LLM completion, tool execution, etc...
+
+        return {
+            "text": "the custom LLM answer",
+            "promptTokens": 46, # Optional
+            "completionTokens": 10, # Optional
+            "estimatedCost": 0.13, # Optional
+            "toolCalls": [],
+        }
